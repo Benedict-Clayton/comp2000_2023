d14915ada44da61e447b6575c22a6e0810e7dec5
Benedict-Clayton <be.layton11@gmail.com>
2023-08-09 05:52:15 +1000

    aa
    aa

--- src/Main.java
+++ src/Main.java
@@ -1,48 +1,39 @@
-import javax.swing.*;
-import java.awt.*;
+import java.awt.Dimension;
+import java.awt.Graphics;
 
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
-public class Main extends JFrame
-{
-    public static void main(String[] args) throws Exception 
-    {
-        Main window = new Main();
-
+public class Main extends JFrame {
+    public static void main(String[] args) throws Exception {
+      Main window = new Main();
+      window.run();
     }
 
-    class Canvas extends JPanel 
-    {
-        private int width = 720;
-        private int height = 720;
+    class Canvas extends JPanel {
+      Grid grid = new Grid();
+      public Canvas() {
+        setPreferredSize(new Dimension(720, 720));
+      }
 
-        public Canvas()
-        {
-            setPreferredSize(new Dimension(width, height));
-        }
-
-        @Override
-        public void paint(Graphics g)
-        {
-            for(int i = 10; i < 710; i+=35)
-            {
-                for(int j = 10; j < 710; j+=35)
-                {
-                    g.setColor(Color.BLACK);
-                    g.drawRect(i, j, 35, 35);
-                    g.setColor(Color.WHITE);
-                    g.fillRect(i, j, 35, 35);
-                }
-            }
-        }
+      @Override
+      public void paint(Graphics g) {
+        grid.paint(g, getMousePosition());
+      }
     }
 
-    private Main()
-    {
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        Canvas canvas = new Canvas();
-        this.setContentPane(canvas);
-        this.pack();
-        this.setVisible(true);
+    private Main() {
+      this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      Canvas canvas = new Canvas();
+      this.setContentPane(canvas);
+      this.pack();
+      this.setVisible(true);
+    }
+
+    public void run() {
+      while(true) {
+        repaint();
+      }
     }
 }
 
