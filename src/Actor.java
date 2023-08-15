4935dc5d981b808ec1e24179100b9c310766e484
Benedict-Clayton <be.layton11@gmail.com>
2023-08-16 02:58:06 +1000

    Trial, I wna see if this'll work during the prac

--- src/Actor.java
+++ src/Actor.java
@@ -12,3 +12,44 @@ public abstract class Actor {
     g.drawRect(loc.x + 5, loc.y + 5, loc.width - 10, loc.height - 10);
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Polygon;
+import java.util.ArrayList;
+
+public abstract class Actor {
+    protected ArrayList<Polygon> polygons; // List of polygons representing the actor
+
+    public Actor() {
+        polygons = new ArrayList<Polygon>();
+    }
+
+    public abstract void initPolygons(); // Initialize polygons for the actor
+
+    public void paint(Graphics g) {
+        for (Polygon polygon : polygons) {
+            g.setColor(Color.BLACK); // Set outline color
+            g.drawPolygon(polygon); // Draw outline
+            g.setColor(getFillColor()); // Set fill color
+            g.fillPolygon(polygon); // Fill with color
+        }
+    }
+
+    protected abstract Color getFillColor(); // Get fill color for the actor
+}
+
+ */
--- src/Cat.java
+++ src/Cat.java
@@ -6,3 +6,55 @@ public class Cat extends Actor {
     color = Color.BLUE;
   }
 }
+
+
+/* 
+~~~~~
+    Polygon ear1 = new Polygon();
+    ear1.addPoint(loc.x + 11, loc.y + 5);
+    ear1.addPoint(loc.x + 15, loc.y + 15);
+    ear1.addPoint(loc.x + 7, loc.y + 15);
+    Polygon ear2 = new Polygon();
+    ear2.addPoint(loc.x + 22, loc.y + 5);
+    ear2.addPoint(loc.x + 26, loc.y + 15);
+    ear2.addPoint(loc.x + 18, loc.y + 15);
+    Polygon face = new Polygon();
+    face.addPoint(loc.x + 5, loc.y + 15);
+    face.addPoint(loc.x + 29, loc.y + 15);
+    face.addPoint(loc.x + 17, loc.y + 30);
+~~~~~
+*/
+
+
+
+
+
+
+
+
+
+
+
+/*
+ public class Cat extends Actor {
+    public Cat() {
+        initPolygons();
+    }
+
+    @Override
+    public void initPolygons() {
+        polygons.clear(); // Clear any existing polygons
+        // Define the polygons that make up the Cat actor
+        // Example: create a triangle for the cat's head
+        int[] xPoints = { 0, 10, 20 };
+        int[] yPoints = { 20, 0, 20 };
+        polygons.add(new Polygon(xPoints, yPoints, 3));
+    }
+
+    @Override
+    protected Color getFillColor() {
+        return Color.BLUE; // Fill color for the Cat actor
+    }
+}
+
+ */
