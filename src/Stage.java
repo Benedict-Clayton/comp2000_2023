f9fe14b66702d3b16401bdc088f9bb362d09844f
Benedict-Clayton <be.layton11@gmail.com>
2023-08-16 02:14:09 +1000

    Updating

--- src/Stage.java
+++ src/Stage.java
@@ -2,15 +2,23 @@ import java.awt.Graphics;
 import java.awt.Point;
 
 public class Stage {
-    Grid grid;
-    Actor cat;
-    Actor dog;
-    Actor bird;
-    
-    public Stage(){
-        grid = new Grid();
-        cat = new cat(grid.cellAtColRow(0,0));
-        dog = new dog(grid.cellAtColRow(1,5));
-        bird = new bird(grid.cellAtColRow(8,6));
-    }
+  Grid grid;
+  Actor cat;
+  Actor dog;
+  Actor bird;
+
+  public Stage() {
+    grid = new Grid();
+    cat = new Cat(grid.cellAtColRow(0, 0));
+    dog = new Dog(grid.cellAtColRow(0, 15));
+    bird = new Bird(grid.cellAtColRow(12, 9));
+  }
+
+  public void paint(Graphics g, Point mouseLoc) {
+    grid.paint(g, mouseLoc);
+    cat.paint(g);
+    dog.paint(g);
+    bird.paint(g);
+  }
 }
+
